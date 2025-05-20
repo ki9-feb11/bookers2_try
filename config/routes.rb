67942e9f2efe01eeb6a5681cbcdd8e0c0f953320
ki9-feb11 <kara.kiku0211@gmial.com>
2025-05-20 07:52:13 +0000

    [Update]ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'homes/top'
   devise_for :users
+  root to: 'homes#top'
+
+  resources :books
+  resource :users, only: [:show, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
